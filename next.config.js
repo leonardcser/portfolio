347e15c84db30094ca1e4cc8836f941791241020
Leonard <73912641+leonardcser@users.noreply.github.com>
2023-02-23 12:16:50 +0100

    Udpate next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ["media.giphy.com"],
-  },
 };
 
 module.exports = nextConfig;
