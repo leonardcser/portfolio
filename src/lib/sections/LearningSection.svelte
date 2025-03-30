b2b86bd84e77e423d50ac7548d976f1d3509ddf5
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-03-30 10:21:32 +0200

    feat: add LearningSection

---
+++ src/lib/sections/LearningSection.svelte
@@ -0,0 +1,21 @@
+<section id="learning">
+	<h2>What I'm Leanring Now</h2>
+	<div class="flex flex-wrap gap-2">
+		<a href="https://memsched.com/leonardcser" target="_blank">
+			<img
+				class="m-0"
+				src="https://memsched.com/api/widgets/02d23572-ad59-4ea2-8a97-ed209251f83b?svg"
+				alt="Understanding Deep Learning - Machine learning book"
+				style="height: 80px;"
+			/>
+		</a>
+		<a href="https://memsched.com/leonardcser" target="_blank">
+			<img
+				class="m-0"
+				src="https://memsched.com/api/widgets/4fa0ef32-09d6-4038-94d8-e7caf8672c0f?svg"
+				alt="LeetCode - Problems Solved"
+				style="height: 75px;"
+			/>
+		</a>
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import Spotlight from '$lib/components/Spotlight.svelte';
 	import HeroSection from '$lib/sections/HeroSection.svelte';
+	import LearningSection from '$lib/sections/LearningSection.svelte';
 	import StorySection from '$lib/sections/StorySection.svelte';
 	import WorksSection from '$lib/sections/WorksSection.svelte';
 </script>
 
 <Spotlight />
 <HeroSection />
+<LearningSection />
 <StorySection />
 <WorksSection />
 <div class="mb-72"></div>
