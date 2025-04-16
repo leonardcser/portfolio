9786c08123532ad6b0a43f8a5fa095e0b1de9769
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-04-16 18:17:58 +0200

    feat: update widgets

--- src/lib/sections/LearningSection.svelte
+++ src/lib/sections/LearningSection.svelte
@@ -5,15 +5,15 @@
 <section id="learning">
 	<h2>What I'm Leanring Now</h2>
 	<div class="not-prose flex flex-wrap gap-2">
-		<WidgetImage
-			widget={{
-				id: '02d23572-ad59-4ea2-8a97-ed209251f83b',
-				title: 'Understanding Deep Learning',
-				subtitle: 'Machine learning book'
-			}}
-			href="https://github.com/leonardcser/understanding-deep-learning"
-			data-umami-event="understanding-deep-learning-widget-link"
-		/>
+		<!-- <WidgetImage -->
+		<!-- 	widget={{ -->
+		<!-- 		id: '02d23572-ad59-4ea2-8a97-ed209251f83b', -->
+		<!-- 		title: 'Understanding Deep Learning', -->
+		<!-- 		subtitle: 'Machine learning book' -->
+		<!-- 	}} -->
+		<!-- 	href="https://github.com/leonardcser/understanding-deep-learning" -->
+		<!-- 	data-umami-event="understanding-deep-learning-widget-link" -->
+		<!-- /> -->
 		<WidgetImage
 			widget={{
 				id: '4fa0ef32-09d6-4038-94d8-e7caf8672c0f',
