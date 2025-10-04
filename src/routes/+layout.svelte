97deef0d86e7f67a9466dbf6777a84ac8346f16a
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-10-04 21:32:19 +0200

    feat: add title and description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,11 @@
 </script>
 
 <svelte:head>
+  <title>Leonard Cseres - Personal Portfolio</title>
+  <meta
+    name="description"
+    content="Explore my personal portfolio, showcasing projects, skills, and achievements in software engineering, development, design, and technology."
+  />
   <link rel="icon" href={favicon} />
 </svelte:head>
 
